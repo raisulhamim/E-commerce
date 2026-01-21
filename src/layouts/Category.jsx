be202d42c72e09b4f0a8c7142970c0b4c0b24ed1
raisulhamim <raisulhamim.dev@gmail.com>
2026-01-21 22:26:45 +0600

    new icone add

--- src/layouts/Category.jsx
+++ src/layouts/Category.jsx
@@ -7,6 +7,11 @@ import Flex from '../components/Flex'
 import CategoryBox from '../components/CategoryBox';
 import { SlScreenSmartphone } from "react-icons/sl";
 import { HiOutlineComputerDesktop } from 'react-icons/hi2'
+import { IoWatchOutline } from "react-icons/io5";
+import { IoCameraOutline } from "react-icons/io5";
+import { MdOutlineHeadphones } from "react-icons/md";
+import { MdOutlineHeadphones } from "react-icons/md";
+import { MdOutlineVideogameAsset } from "react-icons/md";
 
 
 const Category = () => {
@@ -18,10 +23,13 @@ const Category = () => {
                 <Flex className='justify-between pb-[70px]'>
                     <CategoryBox text="Phones" icon={<SlScreenSmartphone />} />
                     <CategoryBox text="Computers" icon={<HiOutlineComputerDesktop />} />
-                    <CategoryBox text="Phones" icon={<SlScreenSmartphone />} />
-                    <CategoryBox text="Computers" icon={<HiOutlineComputerDesktop />} />
-                    <CategoryBox text="Phones" icon={<SlScreenSmartphone />} />
-                    <CategoryBox text="Computers" icon={<HiOutlineComputerDesktop />} />
+                    <CategoryBox text="SmartWatch" icon={<IoWatchOutline />} />
+                    <CategoryBox text="Camera" icon={<IoCameraOutline />
+} />
+                    <CategoryBox text="HeadPhones" icon={<MdOutlineHeadphones />
+} />
+                    <CategoryBox text="Gaming" icon={<MdOutlineVideogameAsset />
+} />
                     
                 </Flex>
                 <hr className='mb-[70px] border-[#0000004d]'/>
